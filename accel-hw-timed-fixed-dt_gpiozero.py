3eb56b5affef9fff41c13d89f8505326631ac7e3
mohamed164 <mohamedallam164@gmail.com>
2024-07-25 16:26:02 +0100

    script with gpiozero updated

--- accel-hw-timed-fixed-dt_gpiozero.py
+++ accel-hw-timed-fixed-dt_gpiozero.py
@@ -2,24 +2,19 @@ import time
 import board
 import busio
 import adafruit_adxl34x
-import os
-import pandas as pd
-import sys
-from gpiozero import Button, PWMOutputDevice
+from gpiozero import Button
 import threading
 
 num_of_samples = 0
 data = []
 previous_time = 0
 accumulated_time = 0
-number_of_files = 0
 
 i2c = busio.I2C(board.SCL, board.SDA)
 accelerometer = adafruit_adxl34x.ADXL345(i2c)
 
 # Setup GPIO pins
 button = Button(16, bounce_time=0.01)
-pwm = PWMOutputDevice(18, frequency=100)
 
 def data_acq_callback():
     global num_of_samples
@@ -42,6 +37,8 @@ def data_acq_callback():
         data_dict = {'timestamp': accumulated_time, 'accX': accel[0], 'accY': accel[1], 'accZ': accel[2]}
         data.append(data_dict)
 
+        print(f"Timestamp: {accumulated_time}, X: {accel[0]}, Y: {accel[1]}, Z: {accel[2]}")
+
         num_of_samples += 1
     
     if num_of_samples == 100:
@@ -54,26 +51,13 @@ if __name__ == '__main__':
 
     # Set the callback for the button press
     button.when_pressed = data_acq_callback
-        
-    while number_of_files != 300:   
-        pwm.value = 0.5  # Start PWM with 50% duty cycle
+    
+    print("Press the button to start data acquisition.")
+
+    try:
         # Wait for data collection to finish
         done_collecting.wait()
-        pwm.off()  # Stop PWM
-        done_collecting.clear()
+    except KeyboardInterrupt:
+        print("Data collection interrupted by user.")
 
-        # Write to a file
-        i = 0
-        while os.path.exists(f'tape_one_side_202310130706_a/tape_one_side.{i}.csv'):
-            i += 1
-
-        with open(f'tape_one_side_202310130706_a/tape_one_side.{i}.csv', "w") as f:
-            df = pd.DataFrame(data)
-            df.to_csv(f, index=False, header=True)
-            f.write("\n")
-
-        data = []       
-        number_of_files += 1
-
-    pwm.off()
-    sys.exit(0)
+    print("Data collection complete.")
