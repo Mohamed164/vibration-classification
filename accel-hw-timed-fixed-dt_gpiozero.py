c99583875d7e10de380f5b50194d73f96c55636e
mohamed164 <mohamedallam164@gmail.com>
2024-07-25 15:57:14 +0100

    script with gpiozero updated

--- accel-hw-timed-fixed-dt_gpiozero.py
+++ accel-hw-timed-fixed-dt_gpiozero.py
@@ -8,33 +8,24 @@ import sys
 from gpiozero import Button, PWMOutputDevice
 import threading
 
-# Initialize global variables
 num_of_samples = 0
 data = []
 previous_time = 0
 accumulated_time = 0
 number_of_files = 0
 
-print("Initializing I2C and accelerometer...")
-
-# Configure I2C and accelerometer
 i2c = busio.I2C(board.SCL, board.SDA)
 accelerometer = adafruit_adxl34x.ADXL345(i2c)
-print("Accelerometer initialized.")
 
-# Initialize button and PWM output
-print("Initializing button and PWM output...")
-button = Button(16, pull_up=True)
+# Setup GPIO pins
+button = Button(16, bounce_time=0.01)
 pwm = PWMOutputDevice(18, frequency=100)
-print("Button and PWM output initialized.")
 
 def data_acq_callback():
     global num_of_samples
     global data
     global previous_time
     global accumulated_time
-
-    print("Button pressed; collecting data...")
 
     if not done_collecting.is_set():
         accel = accelerometer.acceleration
@@ -52,42 +43,36 @@ def data_acq_callback():
         data.append(data_dict)
 
         num_of_samples += 1
-        print(f"Sample {num_of_samples}: {data_dict}")
-
+    
     if num_of_samples == 100:
-        print("100 samples collected; setting done_collecting event.")
         done_collecting.set()
         num_of_samples = 0
-    else:
-        print(f"Samples collected: {num_of_samples}")
 
 if __name__ == '__main__':
     done_collecting = threading.Event()
     done_collecting.clear()
 
-    GPIO.add_event_detect(16, GPIO.RISING, 
-        callback=data_acq_callback, bouncetime=10)
+    button.when_pressed = data_acq_callback
         
     while number_of_files != 300:   
-        p.start(50)
-        #wait for data collection to finish
+        pwm.value = 0.5
+        # Wait for data collection to finish
         done_collecting.wait()
-        p.stop()
+        pwm.off()
         done_collecting.clear()
 
-        #write to a file
+        # Write to a file
         i = 0
-        while os.path.exists(r'tape_one_side_202310130706_a/tape_one_side.%s.csv' % i):
-            i = i + 1
+        while os.path.exists(f'tape_one_side_202310130706_a/tape_one_side.{i}.csv'):
+            i += 1
 
-        with open(r'tape_one_side_202310130706_a/tape_one_side.%s.csv' % i, "w") as f:
+        with open(f'tape_one_side_202310130706_a/tape_one_side.{i}.csv', "w") as f:
             df = pd.DataFrame(data)
             df.to_csv(f, index=False, header=True)
             f.write("\n")
 
         data = []       
-        number_of_files = number_of_files + 1
+        number_of_files += 1
 
-    p.stop()
-    GPIO.cleanup()
+    pwm.off()
     sys.exit(0)
