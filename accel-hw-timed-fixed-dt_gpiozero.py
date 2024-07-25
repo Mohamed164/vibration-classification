27b4988ea33726ea099d945e5704f81384bb0347
mohamed164 <mohamedallam164@gmail.com>
2024-07-25 16:19:42 +0100

    script with gpiozero updated

--- accel-hw-timed-fixed-dt_gpiozero.py
+++ accel-hw-timed-fixed-dt_gpiozero.py
@@ -14,75 +14,66 @@ previous_time = 0
 accumulated_time = 0
 number_of_files = 0
 
-adafruit_adxl34x.DataRate.RATE_3200_HZ
 i2c = busio.I2C(board.SCL, board.SDA)
 accelerometer = adafruit_adxl34x.ADXL345(i2c)
 
-# Use gpiozero for button and LED
-button = Button(16, pull_up=True)  # Button on pin 16 with pull-up resistor
-
-led = PWMOutputDevice(18, frequency=100)  # Create PWM output on pin 18 with 100 Hz frequency
-# Control the LED's brightness (0.0 to 1.0)
-led.value = 0.5  # Set brightness to 50%
-
-
+# Setup GPIO pins
+button = Button(16, bounce_time=0.01)
+pwm = PWMOutputDevice(18, frequency=100)
 
 def data_acq_callback():
-  global num_of_samples
-  global data
-  global previous_time
-  global accumulated_time
+    global num_of_samples
+    global data
+    global previous_time
+    global accumulated_time
 
-  if not done_collecting.is_set():
-    accel = accelerometer.acceleration
-    if num_of_samples == 0:
-      ts = 0
-      accumulated_time = ts
-      previous_time = 0
-    else:
-      current_time = previous_time + 10
-      ts = (current_time - previous_time)
-      accumulated_time = accumulated_time + ts
-      previous_time = current_time
+    if not done_collecting.is_set():
+        accel = accelerometer.acceleration
+        if num_of_samples == 0:
+            ts = 0
+            accumulated_time = ts
+            previous_time = 0
+        else:
+            current_time = previous_time + 10
+            ts = (current_time - previous_time)
+            accumulated_time = accumulated_time + ts
+            previous_time = current_time 
 
-    data_dict = {'timestamp': accumulated_time, 'accX': accel[0], 'accY': accel[1], 'accZ': accel[2]}
-    data.append(data_dict)
+        data_dict = {'timestamp': accumulated_time, 'accX': accel[0], 'accY': accel[1], 'accZ': accel[2]}
+        data.append(data_dict)
 
-    num_of_samples = num_of_samples + 1
-
-  if num_of_samples == 100:
-    done_collecting.set()
-    num_of_samples = 0
-
+        num_of_samples += 1
+    
+    if num_of_samples == 100:
+        done_collecting.set()
+        num_of_samples = 0
 
 if __name__ == '__main__':
-  done_collecting = threading.Event()
-  done_collecting.clear()
-
-  # Use button.when_pressed instead of GPIO interrupt
-  button.when_pressed = data_acq_callback
-
-  while number_of_files != 300:
-    led.start(50)  # Start LED blinking
-    # wait for data collection to finish
-    done_collecting.wait()
-    # led.stop()  # Stop LED blinking
-    # Stop the PWM output
-    led.close()
+    done_collecting = threading.Event()
     done_collecting.clear()
 
-    # write to a file
-    i = 0
-    while os.path.exists(r'tape_one_side_202310130706_a/tape_one_side.%s.csv' % i):
-      i = i + 1
+    # Set the callback for the button press
+    button.when_pressed = data_acq_callback
+        
+    while number_of_files != 300:   
+        pwm.value = 0.5  # Start PWM with 50% duty cycle
+        # Wait for data collection to finish
+        done_collecting.wait()
+        pwm.off()  # Stop PWM
+        done_collecting.clear()
 
-    with open(r'tape_one_side_202310130706_a/tape_one_side.%s.csv' % i, "w") as f:
-      df = pd.DataFrame(data)
-      df.to_csv(f, index=False, header=True)
-      f.write("\n")
+        # Write to a file
+        i = 0
+        while os.path.exists(f'tape_one_side_202310130706_a/tape_one_side.{i}.csv'):
+            i += 1
 
-    data = []
-    number_of_files = number_of_files + 1
+        with open(f'tape_one_side_202310130706_a/tape_one_side.{i}.csv', "w") as f:
+            df = pd.DataFrame(data)
+            df.to_csv(f, index=False, header=True)
+            f.write("\n")
 
-  led.close()  # Ensure LED is off at the end
-  sys.exit(0)
+        data = []       
+        number_of_files += 1
+
+    pwm.off()
+    sys.exit(0)
