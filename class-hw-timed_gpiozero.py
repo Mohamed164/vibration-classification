0066b7726e21b5767bf57374ca59d8d0a1cc59d2
mohamed164 <mohamedallam164@gmail.com>
2024-07-26 12:28:59 +0100

    classification script

---
+++ class-hw-timed_gpiozero.py
@@ -0,0 +1,168 @@
+import os
+import sys
+import signal
+import time
+import io
+import threading
+import getopt
+import pandas as pd
+import numpy as np
+import busio
+import board
+import adafruit_adxl34x
+import akida
+import scipy
+from spectral_analysis import generate_features
+from gpiozero import Button, PWMOutputDevice
+
+# Variables
+num_of_samples = 0
+data = []
+previous_time = 0
+accumulated_time = 0
+number_of_files = 0
+accel0 = []
+accel1 = []
+accel2 = []
+features = []
+
+# Accelerometer setup
+i2c = busio.I2C(board.SCL, board.SDA)
+accelerometer = adafruit_adxl34x.ADXL345(i2c)
+
+# GPIO setup
+button = Button(16)
+pwm = PWMOutputDevice(18, frequency=100)
+done_collecting = threading.Event()
+
+def dsp(features):
+    processed_features = []
+
+    implementation_version = 4
+    draw_graphs = False
+    raw_data = np.array(features)
+    axes = ['x', 'y', 'z']
+    sampling_freq = 100
+    scale_axes = 1
+    input_decimation_ratio = 1
+    filter_type = 'none'
+    filter_cutoff = 0
+    filter_order = 0
+    analysis_type = 'FFT'
+    fft_length = 16
+    spectral_peaks_count = 0
+    spectral_peaks_threshold = 0
+    spectral_power_edges = "0"
+    do_log = True
+    do_fft_overlap = True
+    extra_low_freq = False
+    wavelet_level = 1
+    wavelet = ""
+
+    output = generate_features(implementation_version, draw_graphs, raw_data, axes, sampling_freq, scale_axes, input_decimation_ratio,
+                        filter_type, filter_cutoff, filter_order, analysis_type, fft_length, spectral_peaks_count,
+                        spectral_peaks_threshold, spectral_power_edges, do_log, do_fft_overlap,
+                        wavelet_level, wavelet, extra_low_freq)
+
+    idx = 0
+    for axis in axes:
+        for label in output['labels']:
+            processed_features.append(output["features"][idx])
+            idx += 1
+    
+    processed_features_np = np.array(processed_features)
+    processed_features_np_32 = np.float32(processed_features_np)
+
+    return processed_features_np_32
+
+def akida_model_inference(model, processed_features):
+    scaling_factor = 15 / np.max(processed_features)
+    processed_features_uint8 = np.uint8(processed_features * scaling_factor)
+    input_shape = (1,) + tuple(model.input_shape)
+    inputs = processed_features_uint8.reshape(input_shape)
+    device = akida.devices()[0]
+    model.map(device)
+    results = model.predict(inputs)
+    return results
+
+def data_acq_callback():
+    global num_of_samples
+    global data
+    global previous_time
+    global accumulated_time
+
+    if not done_collecting.is_set():
+        accel = accelerometer.acceleration
+        if num_of_samples == 0:
+            ts = 0
+            accumulated_time = ts
+            previous_time = 0
+        else:
+            current_time = previous_time + 10
+            ts = (current_time - previous_time)
+            accumulated_time = accumulated_time + ts
+            previous_time = current_time 
+
+        data.append(accel[0])
+        data.append(accel[1])
+        data.append(accel[2])
+
+        num_of_samples += 1
+    
+    if num_of_samples == 100:
+        done_collecting.set()
+        num_of_samples = 0
+
+def signal_handler(sig, frame):
+    print('Interrupted')
+    pwm.off()
+    sys.exit(0)
+
+signal.signal(signal.SIGINT, signal_handler)
+
+def help():
+    print('python3 claa-hw-time.py <path-to-akida-model.fbz>')
+
+def main(argv):
+    try:
+        opts, args = getopt.getopt(argv, "h", ["--help"])
+    except getopt.GetoptError:
+        help()
+        sys.exit(2)
+
+    for opt, arg in opts:
+        if opt in ('-h', '--help'):
+            help()
+            sys.exit()
+
+    if len(args) != 1:
+        help()
+        sys.exit(2)
+
+    model = args[0]
+    dir_path = os.path.dirname(os.path.realpath(__file__))
+    modelfile = os.path.join(dir_path, model)
+
+    pwm.value = 0.5  # Start PWM with 50% duty cycle
+    done_collecting.wait()
+    pwm.off()
+
+    features = data
+
+    processed_features = dsp(features)
+
+    akida_model = akida.Model(modelfile)
+    predictions = akida_model_inference(akida_model, processed_features)
+
+    print(predictions)
+    np.set_printoptions(suppress=True, precision=6)
+    softmaxed_pred = scipy.special.softmax(predictions)
+    print(softmaxed_pred.shape)
+    print("full: ", softmaxed_pred[0][0][0][0])
+    print("nominal: ", softmaxed_pred[0][0][0][1])
+    print("one side: ", softmaxed_pred[0][0][0][2])
+    print("wire: ", softmaxed_pred[0][0][0][3])
+
+if __name__ == '__main__':
+    button.when_pressed = data_acq_callback
+    main(sys.argv[1:])
